f6b6da09801149990b8f91ccd78c054fafd19666
Aaron <aaron6yi@gmail.com>
2025-01-12 12:10:36 +0800

    Add files via upload

--- sample.c
+++ sample.c
@@ -1,9 +1,20 @@
 #include <string.h>
 #include <stdio.h>
 #include <stdlib.h>
- 
+
 #define BUFSIZE 256
-    
+
+// Escape special characters in the file path to prevent command injection
+void escapeShellArg(char* dest, const char* src) {
+    while (*src) {
+        if (*src == '\'' || *src == '"' || *src == '\\') {
+            *dest++ = '\\';  // Escape the special character
+        }
+        *dest++ = *src++;
+    }
+    *dest = '\0';
+}
+
 // This program prints the size of a specified file in bytes
 int main(int argc, char** argv) {
     // Ensure that the user supplied exactly one command line argument
@@ -11,7 +22,22 @@ int main(int argc, char** argv) {
         fprintf(stderr, "Please provide the address of a file as an input.\n");
         return -1;
     }
-    char cmd[BUFSIZE] = "wc -c < ";
-    strcat(cmd, argv[1]);
-    system(cmd);
+
+    // Prepare the command and escape the file path
+    char cmd[BUFSIZE] = "wc";
+    char escapedFile[BUFSIZE];
+    
+    // Escape the user input to prevent injection
+    escapeShellArg(escapedFile, argv[1]);
+    
+    // Set up the command arguments
+    char* cmdArgs[] = {cmd, "-c", escapedFile, NULL};
+    
+    // Use execvp to execute the command securely
+    if (execvp(cmd, cmdArgs) == -1) {
+        perror("Error executing command");
+        return -1;
+    }
+
+    return 0;
 }
